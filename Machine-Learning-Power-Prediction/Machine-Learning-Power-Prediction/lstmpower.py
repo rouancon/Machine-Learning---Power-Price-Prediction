b02a579f6705070434f4cb09109e394c6153e6f4
rouancon <rouan.c@husky.neu.edu>
2018-06-17 21:06:05 -0400

    Final Commit

--- Machine-Learning-Power-Prediction/Machine-Learning-Power-Prediction/lstmpower.py
+++ Machine-Learning-Power-Prediction/Machine-Learning-Power-Prediction/lstmpower.py
@@ -11,15 +11,11 @@ import matplotlib.pyplot as plt
 
 #get datasets from excel file
 price2015=[]
-load2015=[]
 price2016=[]
-load2016=[]
 price2017=[]
-load2017=[]
-day=[[0 for x in range(24)] for y in range(365)]
 workbook = xlrd.open_workbook('All-Power-Pricing-Data.xlsx')
 
-#----------IMPORT DATA---------
+#----------IMPORT/CLEAN DATA---------
 #2015
 worksheet = workbook.sheet_by_name('2015')
 done=False
@@ -36,24 +32,13 @@ for i in range(0,365):
         price2015x[i][j]=price2015[i*24+j]
 
 price2015x=np.array(price2015x)
-print(price2015x)
 
 price2015y=[[0] for y in range(365)]
 for i in range(0,365):
     for j in range(23,24):
-        price2015y[i][j]=price2015[i*24+j+1]
+        price2015y[i][j-23]=price2015[i*24+j]
 
 price2015y=np.array(price2015y)
-print(price2015y)
-
-done=False
-cell=1
-while (not done):
-    load2015.append(worksheet.cell(cell, 2).value)
-    cell=cell+1
-    if worksheet.cell(cell, 1).value == 0:
-        done=True
-        load2015=np.array(load2015)
 
 #2016
 worksheet = workbook.sheet_by_name('2016')
@@ -66,14 +51,19 @@ while (not done):
         done=True
         price2016=np.array(price2016)
 
-done=False
-cell=1
-while (not done):
-    load2016.append(worksheet.cell(cell, 2).value)
-    cell=cell+1
-    if worksheet.cell(cell, 1).value == 0:
-        done=True
-        load2016=np.array(load2016)
+price2016x=[[0 for x in range(23)] for y in range(365)]
+for i in range(0,365):
+    for j in range(0,23):
+        price2016x[i][j]=price2016[i*24+j]
+
+price2016x=np.array(price2016x)
+
+price2016y=[[0] for y in range(365)]
+for i in range(0,365):
+    for j in range(23,24):
+        price2016y[i][j-23]=price2016[i*24+j]
+
+price2016y=np.array(price2016y)
 
 #2017
 worksheet = workbook.sheet_by_name('2017')
@@ -86,49 +76,48 @@ while (not done):
         done=True
         price2017=np.array(price2017)
 
-done=False
-cell=1
-while (not done):
-    load2017.append(worksheet.cell(cell, 2).value)
-    cell=cell+1
-    if worksheet.cell(cell, 1).value == 0:
-        done=True
-        load2017=np.array(load2017)
-
+price2017x=[[0 for x in range(23)] for y in range(365)]
 for i in range(0,365):
-    for j in range(0,24):
-        day[i][j]=j+1
+    for j in range(0,23):
+        price2017x[i][j]=price2017[i*24+j]
 
-day=np.array(day)
-print(day)
+price2017x=np.array(price2017x)
+
+price2017y=[[0] for y in range(365)]
+for i in range(0,365):
+    for j in range(23,24):
+        price2017y[i][j-23]=price2017[i*24+j]
+
+price2017y=np.array(price2017y)
 
 
 #----------DEFINE MODEL------------
 model = Sequential()
-model.add(LSTM(32, input_shape=(24,1)))
-model.add(Dense(24))
-model.add(Dense(24))
-model.compile(loss='mse', optimizer='rmsprop', metrics = ['accuracy'])
+model.add(LSTM(115, input_shape=(23,1)))
+model.add(Dense(2, activation='relu'))
+model.add(Dense(1))
+model.compile(loss='mse', optimizer='rmsprop', metrics = ['mean_squared_error'])
 
 #train and validate model
-#load2015 = np.reshape(load2015, (365,24,1))
-#load2016 = np.reshape(load2016, (365,24,1))
-#load2017 = np.reshape(load2017, (365,24,1))
-day = np.reshape(day, (365,24,1))
-price2015 = np.reshape(price2015, (365,24))
-price2016 = np.reshape(price2016, (365,24))
-model.fit(x=day, y=price2015, epochs=30, batch_size=32, verbose=2, validation_split=0, validation_data=(day,price2016), shuffle=False)
+price2015x = np.reshape(price2015x, (365,23,1))
+price2016x = np.reshape(price2017x, (365,23,1))
+price2017x = np.reshape(price2017x, (365,23,1))
+price2015y = np.reshape(price2015y, (365,1))
+price2016y = np.reshape(price2017y, (365,1))
+price2017y = np.reshape(price2017y, (365,1))
+model.fit(x=price2015x, y=price2015y, epochs=200, batch_size=32, verbose=2, validation_split=0, validation_data=(price2016x,price2016y), shuffle=False)
 
-#model results
-result = model.predict(x=day)
+#get model results
+result = model.predict(x=price2017x)
 
 #reshape array(s) back
-result = np.reshape(result, (8760,1))
+result = np.reshape(result, (365,1))
 print(result)
+print(price2017y)
 
 
 #----------GRAPH RESULTS----------
-plt.plot(price2017, 'r', result, 'b')
+plt.plot(price2017y, 'r', result, 'b')
 plt.show()
 
 #---------------------------------
